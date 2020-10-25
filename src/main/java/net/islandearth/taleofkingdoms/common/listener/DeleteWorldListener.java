008f7f8bf84ce20ec2d5e2bc2a12403536fd7c68
SamB440 <sam@islandearth.net>
2020-10-25 16:27:47 +0000

    Ignore files that don't exist

--- src/main/java/net/islandearth/taleofkingdoms/common/listener/DeleteWorldListener.java
+++ src/main/java/net/islandearth/taleofkingdoms/common/listener/DeleteWorldListener.java
@@ -11,7 +11,7 @@ public class DeleteWorldListener extends Listener {
     public DeleteWorldListener() {
         WorldDeleteCallback.EVENT.register(worldName -> {
             File file = new File(TaleOfKingdoms.getAPI().map(TaleOfKingdomsAPI::getDataFolder).orElseThrow(() -> new IllegalArgumentException("API not present")) + "worlds/" + worldName + ".conquestworld");
-            if (!file.delete()) {
+            if (!file.delete() && file.exists()) {
                 TaleOfKingdoms.LOGGER.error("Unable to delete " + worldName + ".conquestworld file");
             }
         });
