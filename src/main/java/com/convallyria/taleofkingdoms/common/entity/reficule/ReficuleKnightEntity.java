36fe5d442742fd3a2d335d5b183cec071541a6a0
SamB440 <sam@islandearth.net>
2021-04-07 21:29:35 +0100

    Cleanup
    Signed-off-by: SamB440 <sam@islandearth.net>

--- src/main/java/com/convallyria/taleofkingdoms/common/entity/reficule/ReficuleKnightEntity.java
+++ src/main/java/com/convallyria/taleofkingdoms/common/entity/reficule/ReficuleKnightEntity.java
@@ -1,24 +1,25 @@
 package com.convallyria.taleofkingdoms.common.entity.reficule;
 
+import com.convallyria.taleofkingdoms.common.entity.EntityTypes;
 import com.convallyria.taleofkingdoms.common.entity.TOKEntity;
-import com.convallyria.taleofkingdoms.common.entity.ai.goal.WanderAroundGuildGoal;
-import com.convallyria.taleofkingdoms.common.entity.guild.GuildGuardEntity;
+import com.convallyria.taleofkingdoms.common.entity.ai.goal.ImprovedFollowTargetGoal;
+import com.convallyria.taleofkingdoms.common.entity.ai.goal.TeleportTowardsPlayerGoal;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.goal.FollowTargetGoal;
 import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.WanderAroundGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.mob.Monster;
 import net.minecraft.entity.mob.PathAwareEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 
-public class ReficuleKnightEntity extends TOKEntity {
+public class ReficuleKnightEntity extends TOKEntity implements Monster, TeleportAbility {
 
     public ReficuleKnightEntity(@NotNull EntityType<? extends PathAwareEntity> entityType, @NotNull World world) {
         super(entityType, world);
@@ -30,18 +31,29 @@ public class ReficuleKnightEntity extends TOKEntity {
     @Override
     protected void initGoals() {
         super.initGoals();
-        this.goalSelector.add(3, new WanderAroundGuildGoal(this, 0.6D));
-        this.targetSelector.add(2, new FollowTargetGoal<>(this, LivingEntity.class, 100, true, true, livingEntity -> {
-            return livingEntity instanceof PlayerEntity || livingEntity instanceof GuildGuardEntity; // Test whether guild guards are attacked
+        this.targetSelector.add(1, new ImprovedFollowTargetGoal<>(this, EntityType.PLAYER, true));
+        this.targetSelector.add(2, new ImprovedFollowTargetGoal<>(this, EntityTypes.GUILDGUARD, true));
+        this.targetSelector.add(3, new TeleportTowardsPlayerGoal(this, entity -> {
+            return entity.squaredDistanceTo(this) < this.getAttributeValue(EntityAttributes.GENERIC_FOLLOW_RANGE);
         }));
-        this.goalSelector.add(1, new MeleeAttackGoal(this, 0.6D, false));
+        this.goalSelector.add(2, new WanderAroundGoal(this, 0.6D));
+        this.goalSelector.add(1, new MeleeAttackGoal(this, 0.8D, false));
     }
 
     public static DefaultAttributeContainer.Builder createMobAttributes() { // Slightly higher stats than guild guards.
         return TOKEntity.createMobAttributes()
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 40.0D)
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 75.0D) // Big increase! Needs balancing?
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 16.0D)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 12.0D)
                 .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 1.5D);
+    }
+
+    @Override
+    public boolean teleportTo(Entity entity) {
+        double x = entity.getX();
+        double y = entity.getY();
+        double z = entity.getZ();
+        return this.teleport(x, y, z, true);
     }
 
     @Override
