352c450658197897ffc66aac3579918902cad771
SamB440 <sam@islandearth.net>
2021-04-17 19:34:52 +0100

    Add scaling to blacksmith gui
    Signed-off-by: SamB440 <sam@islandearth.net>

--- src/main/java/com/convallyria/taleofkingdoms/client/gui/entity/BlacksmithScreen.java
+++ src/main/java/com/convallyria/taleofkingdoms/client/gui/entity/BlacksmithScreen.java
@@ -6,6 +6,7 @@ import com.convallyria.taleofkingdoms.client.gui.entity.widget.PageTurnWidget;
 import com.convallyria.taleofkingdoms.client.gui.entity.widget.ShopButtonWidget;
 import com.convallyria.taleofkingdoms.client.gui.entity.widget.ShopScreenInterface;
 import com.convallyria.taleofkingdoms.client.gui.image.Image;
+import com.convallyria.taleofkingdoms.client.gui.image.ScaleSize;
 import com.convallyria.taleofkingdoms.client.gui.shop.Shop;
 import com.convallyria.taleofkingdoms.client.gui.shop.ShopPage;
 import com.convallyria.taleofkingdoms.client.translation.Translations;
@@ -44,6 +45,7 @@ import com.convallyria.taleofkingdoms.common.shop.WoodenShovelShopItem;
 import com.convallyria.taleofkingdoms.common.shop.WoodenSwordShopItem;
 import com.convallyria.taleofkingdoms.common.world.ClientConquestInstance;
 import com.google.common.collect.ImmutableList;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
@@ -52,6 +54,7 @@ import net.minecraft.util.Identifier;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class BlacksmithScreen extends ScreenTOK implements ShopScreenInterface {
 
@@ -63,11 +66,21 @@ public class BlacksmithScreen extends ScreenTOK implements ShopScreenInterface {
     private ShopItem selectedItem;
     private Shop shop;
 
+    private static final ImmutableList<ScaleSize> SCALE_SIZES = ImmutableList.of(
+            new ScaleSize(1, 810, 265),
+            new ScaleSize(2, 310, 113),
+            new ScaleSize(3, 170, 55),
+            new ScaleSize(4, 100, 27));
+
+    private static final ImmutableList<ScaleSize> SCALE_SIZES_TWO = ImmutableList.of(
+            new ScaleSize(1, 1000, 265),
+            new ScaleSize(2, 540, 113),
+            new ScaleSize(3, 360, 55),
+            new ScaleSize(4, 275, 27));
+
     public BlacksmithScreen(PlayerEntity player, BlacksmithEntity entity, ClientConquestInstance instance) {
         super("menu.taleofkingdoms.blacksmith.name");
         this.player = player;
-        addImage(new Image(new Identifier(TaleOfKingdoms.MODID, "textures/gui/menu1.png"), this.width / 2 + 310, this.height / 2 + 95, new int[]{230, 230}));
-        addImage(new Image(new Identifier(TaleOfKingdoms.MODID, "textures/gui/menu2.png"), this.width / 2 + 540, this.height / 2 + 95, new int[]{230, 230}));
         this.entity = entity;
         this.instance = instance;
         this.shopItems = ImmutableList.of(new ArrowShopItem(), new BowShopItem(),
@@ -77,8 +90,20 @@ public class BlacksmithScreen extends ScreenTOK implements ShopScreenInterface {
                 new IronAxeShopItem(), new IronBootsShopItem(), new IronChestplateShopItem(), new IronHelmetShopItem(),
                 new IronLeggingsShopItem(), new IronPickaxeShopItem(), new IronShovelShopItem(), new IronSwordShopItem(),
                 new LeatherBootsShopItem(), new LeatherChestplateShopItem(), new LeatherHelmetShopItem(), new LeatherLeggingsShopItem(),
-                new ShieldShopItem(),new StoneAxeShopItem(), new StonePickaxeShopItem(), new StoneShovelShopItem(), new StoneSwordShopItem(),
+                new ShieldShopItem(), new StoneAxeShopItem(), new StonePickaxeShopItem(), new StoneShovelShopItem(), new StoneSwordShopItem(),
                 new WoodenAxeShopItem(), new WoodenPickaxeShopItem(), new WoodenShovelShopItem(), new WoodenSwordShopItem());
+        int guiScale = MinecraftClient.getInstance().options.guiScale;
+        TaleOfKingdoms.LOGGER.info("a" + guiScale);
+        Optional<ScaleSize> scaleSize = SCALE_SIZES.stream().filter(size -> size.getGuiScale() == guiScale).findFirst();
+        if (!scaleSize.isPresent()) return;
+        int x = scaleSize.get().getX();
+        int y = scaleSize.get().getY();
+        Optional<ScaleSize> scaleSizeTwo = SCALE_SIZES_TWO.stream().filter(size -> size.getGuiScale() == guiScale).findFirst();
+        if (!scaleSizeTwo.isPresent()) return;
+        int xTwo = scaleSizeTwo.get().getX();
+        int yTwo = scaleSizeTwo.get().getY();
+        addImage(new Image(new Identifier(TaleOfKingdoms.MODID, "textures/gui/menu1.png"), x, y, new int[]{230, 230}));
+        addImage(new Image(new Identifier(TaleOfKingdoms.MODID, "textures/gui/menu2.png"), xTwo, yTwo, new int[]{230, 230}));
     }
 
     @Override
