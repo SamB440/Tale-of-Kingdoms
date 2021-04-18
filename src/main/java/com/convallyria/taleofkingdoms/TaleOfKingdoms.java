ecd9fd9e7e048e4599f11e3d118ae815c02be478
SamB440 <sam@islandearth.net>
2021-04-18 21:08:01 +0100

    Add ability to sell items
    Signed-off-by: SamB440 <sam@islandearth.net>

--- src/main/java/com/convallyria/taleofkingdoms/TaleOfKingdoms.java
+++ src/main/java/com/convallyria/taleofkingdoms/TaleOfKingdoms.java
@@ -1,5 +1,8 @@
 package com.convallyria.taleofkingdoms;
 
+import com.convallyria.taleofkingdoms.client.gui.shop.SellScreenHandler;
+import com.convallyria.taleofkingdoms.common.block.SellBlock;
+import com.convallyria.taleofkingdoms.common.block.entity.SellBlockEntity;
 import com.convallyria.taleofkingdoms.common.entity.EntityTypes;
 import com.convallyria.taleofkingdoms.common.entity.generic.HunterEntity;
 import com.convallyria.taleofkingdoms.common.entity.generic.KnightEntity;
@@ -36,16 +39,23 @@ import com.convallyria.taleofkingdoms.common.schematic.Schematic;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParseException;
+import com.google.gson.reflect.TypeToken;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
 import net.fabricmc.fabric.api.structure.v1.FabricStructureBuilder;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.command.argument.TextArgumentType;
 import net.minecraft.entity.Entity;
+import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.structure.StructurePieceType;
 import net.minecraft.structure.processor.StructureProcessorType;
@@ -67,6 +77,11 @@ import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
@@ -92,6 +107,32 @@ public class TaleOfKingdoms implements ModInitializer {
     private static final ConfiguredStructureFeature<?, ?> GATEWAY_CONFIGURED = GATEWAY_STRUCTURE.configure(DefaultFeatureConfig.DEFAULT);
 
     public static final StructureProcessorType<?> GATEWAY_PROCESSOR = StructureProcessorType.register("gateway", GatewayStructureProcessor.CODEC);
+
+    public static final ScreenHandlerType<SellScreenHandler> SELL_SCREEN_HANDLER;
+
+    public static final Block SELL_BLOCK;
+    public static final BlockEntityType<SellBlockEntity> SELL_BLOCK_ENTITY;
+
+    // a public identifier for multiple parts of our bigger chest
+    public static final Identifier SELL_BLOCK_IDENTIFIER = new Identifier(MODID, "sell_block");
+
+    static {
+        //We use registerSimple here because our Entity is not an ExtendedScreenHandlerFactory
+        //but a NamedScreenHandlerFactory.
+        //In a later Tutorial you will see what ExtendedScreenHandlerFactory can do!
+        SELL_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(new Identifier(TaleOfKingdoms.MODID, "sell_screen"), SellScreenHandler::new);
+
+        SELL_BLOCK = Registry.register(Registry.BLOCK, SELL_BLOCK_IDENTIFIER, new SellBlock(FabricBlockSettings.copyOf(Blocks.CHEST)));
+
+        //The parameter of build at the very end is always null, do not worry about it
+        SELL_BLOCK_ENTITY = Registry.register(Registry.BLOCK_ENTITY_TYPE, SELL_BLOCK_IDENTIFIER, BlockEntityType.Builder.create(SellBlockEntity::new, SELL_BLOCK).build(null));
+    }
+
+    private Map<String, Integer> itemValues;
+
+    public Map<String, Integer> getItemValues() {
+        return itemValues;
+    }
 
     @Override
     public void onInitialize() {
@@ -126,6 +167,18 @@ public class TaleOfKingdoms implements ModInitializer {
         FabricDefaultAttributeRegistry.register(EntityTypes.REFICULE_SOLDIER, ReficuleSoldierEntity.createMobAttributes());
         FabricDefaultAttributeRegistry.register(EntityTypes.REFICULE_GUARDIAN, ReficuleGuardianEntity.createMobAttributes());
         FabricDefaultAttributeRegistry.register(EntityTypes.REFICULE_MAGE, ReficuleMageEntity.createMobAttributes());
+
+        // Load item values
+        InputStream inputStream = TaleOfKingdoms.class.getResourceAsStream("/assets/shop/values.json");
+        if (inputStream == null) {
+            LOGGER.warn("Cannot read shop values file! inputStream is null.");
+            return;
+        }
+        InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        Type type = new TypeToken<Map<String, Integer>>(){}.getType();
+        Map<String, Integer> mapValue = getGson().fromJson(reader, type);
+        mapValue.forEach((name, value) -> LOGGER.info("Loaded item value " + name + " (" + value + ")."));
+        this.itemValues = mapValue;
     }
 
     /**
--- src/main/java/com/convallyria/taleofkingdoms/client/gui/entity/BlacksmithScreen.java
+++ src/main/java/com/convallyria/taleofkingdoms/client/gui/entity/BlacksmithScreen.java
@@ -45,12 +45,16 @@ import com.convallyria.taleofkingdoms.common.shop.WoodenShovelShopItem;
 import com.convallyria.taleofkingdoms.common.shop.WoodenSwordShopItem;
 import com.convallyria.taleofkingdoms.common.world.ClientConquestInstance;
 import com.google.common.collect.ImmutableList;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -93,7 +97,6 @@ public class BlacksmithScreen extends ScreenTOK implements ShopScreenInterface {
                 new ShieldShopItem(), new StoneAxeShopItem(), new StonePickaxeShopItem(), new StoneShovelShopItem(), new StoneSwordShopItem(),
                 new WoodenAxeShopItem(), new WoodenPickaxeShopItem(), new WoodenShovelShopItem(), new WoodenSwordShopItem());
         int guiScale = MinecraftClient.getInstance().options.guiScale;
-        TaleOfKingdoms.LOGGER.info("a" + guiScale);
         Optional<ScaleSize> scaleSize = SCALE_SIZES.stream().filter(size -> size.getGuiScale() == guiScale).findFirst();
         if (!scaleSize.isPresent()) return;
         int x = scaleSize.get().getX();
@@ -123,7 +126,26 @@ public class BlacksmithScreen extends ScreenTOK implements ShopScreenInterface {
             selectedItem.buy(instance, player);
         }));
 
-        this.addButton(new ButtonWidget(this.width / 2 + 132, this.height / 2 - 30 , 55, 20, new LiteralText("Sell"), button -> this.onClose()));
+        this.addButton(new ButtonWidget(this.width / 2 + 132, this.height / 2 - 30 , 55, 20, new LiteralText("Sell"), button -> {
+            /*
+             * WHY is this what we need to do for a proper sell GUI?
+             * I HATE THIS!!!!
+             * someone please rewrite it so blocks are not needed
+             */
+            BlockPos pos = entity.getBlockPos().add(0, 2, 0);
+            TaleOfKingdoms.getAPI().ifPresent(api -> {
+                api.getScheduler().queue(server -> {
+                    ServerPlayerEntity serverPlayer = server.getPlayerManager().getPlayer(player.getUuid());
+                    server.getOverworld().setBlockState(pos, TaleOfKingdoms.SELL_BLOCK.getDefaultState());
+                    BlockState state = server.getOverworld().getBlockState(pos);
+                    NamedScreenHandlerFactory screenHandlerFactory = state.createScreenHandlerFactory(server.getOverworld(), pos);
+                    if (screenHandlerFactory != null) {
+                        //With this call the server will request the client to open the appropriate Screenhandler
+                        serverPlayer.openHandledScreen(screenHandlerFactory);
+                    }
+                }, 1);
+            });
+        }));
         this.addButton(new PageTurnWidget(this.width / 2 - 135, this.height / 2 - 100, false, button -> shop.previousPage(), true));
         this.addButton(new PageTurnWidget(this.width / 2 + 130, this.height / 2 - 100, true, button -> shop.nextPage(), true));
         this.addButton(new ButtonWidget(this.width / 2 - 160 , this.height / 2 + 20, 45, 20, new LiteralText("Exit"), button -> this.onClose()));
