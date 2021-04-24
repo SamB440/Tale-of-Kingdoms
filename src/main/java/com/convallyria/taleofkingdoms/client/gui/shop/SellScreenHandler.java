3f344660291479d2d95c5218c07df7d9e8587254
SamB440 <sam@islandearth.net>
2021-04-24 17:49:03 +0100

    Fix shift clicking on sell screen
    Signed-off-by: SamB440 <sam@islandearth.net>

--- src/main/java/com/convallyria/taleofkingdoms/client/gui/shop/SellScreenHandler.java
+++ src/main/java/com/convallyria/taleofkingdoms/client/gui/shop/SellScreenHandler.java
@@ -81,26 +81,6 @@ public class SellScreenHandler extends ScreenHandler {
     // Shift + Player Inv Slot
     @Override
     public ItemStack transferSlot(PlayerEntity player, int invSlot) {
-        ItemStack newStack = ItemStack.EMPTY;
-        Slot slot = this.slots.get(invSlot);
-        if (slot != null && slot.hasStack()) {
-            ItemStack originalStack = slot.getStack();
-            newStack = originalStack.copy();
-            if (invSlot < this.inventory.size()) {
-                if (!this.insertItem(originalStack, this.inventory.size(), this.slots.size(), true)) {
-                    return ItemStack.EMPTY;
-                }
-            } else if (!this.insertItem(originalStack, 0, this.inventory.size(), false)) {
-                return ItemStack.EMPTY;
-            }
-
-            if (originalStack.isEmpty()) {
-                slot.setStack(ItemStack.EMPTY);
-            } else {
-                slot.markDirty();
-            }
-        }
-
-        return newStack;
+        return ItemStack.EMPTY;
     }
 }
