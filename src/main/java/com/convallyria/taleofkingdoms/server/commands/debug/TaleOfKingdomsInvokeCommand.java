9a75ddff3847ad507baa05b59f317047d9e42796
Jordan Adams <jordan@jordanplayz158.me>
2021-05-13 15:49:17 -0400

    Changes: - Made commands into separate classes - Added debug, add, get, invoke, and set commands for easier debugging

---
+++ src/main/java/com/convallyria/taleofkingdoms/server/commands/debug/TaleOfKingdomsInvokeCommand.java
@@ -0,0 +1,83 @@
+package com.convallyria.taleofkingdoms.server.commands.debug;
+
+import com.convallyria.taleofkingdoms.TaleOfKingdoms;
+import com.convallyria.taleofkingdoms.client.translation.Translations;
+import com.convallyria.taleofkingdoms.common.world.ConquestInstance;
+import com.convallyria.taleofkingdoms.common.world.ServerConquestInstance;
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.entity.Entity;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Texts;
+import net.minecraft.util.Util;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.UUID;
+
+public class TaleOfKingdomsInvokeCommand implements Command<ServerCommandSource> {
+    @Override
+    public int run(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        Entity entity = context.getSource().getEntity();
+        if (entity != null) {
+            String message = "{\"text\":\"List of invoke events: saveVillagers, guildAttack\"}";
+            entity.sendSystemMessage(Texts.parse(context.getSource(), TaleOfKingdoms.parse(new StringReader(message)), entity, 0), Util.NIL_UUID);
+            return 1;
+        }
+        return 0;
+    }
+
+    public static int invokeSaveVillagers(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        try {
+            ServerPlayerEntity player = context.getSource().getPlayer();
+
+            Translations.LONE_HELP.send(player);
+
+            BlockPos structureLocation = player.getServerWorld().locateStructure(TaleOfKingdoms.REFICULE_VILLAGE_STRUCTURE, player.getBlockPos(), 100, false);
+            String message = "[\"\",{\"text\":\"Do you wish to \"},{\"text\":\"teleport\",\"bold\":true,\"underlined\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/tp " + structureLocation.getX() + " 100 " + structureLocation.getZ() + "\"}},{\"text\":\" to the village?\"}]";
+            player.sendSystemMessage(Texts.parse(context.getSource(), TaleOfKingdoms.parse(new StringReader(message)), player, 0), Util.NIL_UUID);
+
+            return 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static int invokeGuildAttack(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        try {
+            ConquestInstance instance = TaleOfKingdoms.getAPI().get().getConquestInstanceStorage().mostRecentInstance().get();
+            ServerPlayerEntity player = context.getSource().getPlayer();
+            UUID playerUuid = player.getUuid();
+
+            // Pre-requisites for attack
+
+            // Requires at least 750 worthiness (using 800 just to be safe)
+            instance.addWorthiness(playerUuid, 750);
+            instance.setUnderAttack(false);
+
+            // Saving a temporary variable and setting it back to temporary variable as the internal logic of ConquestInstance#attack does not handle it.
+            boolean hasRebuilt = instance.hasRebuilt();
+            instance.setRebuilt(false);
+
+            instance.attack(player, player.getServerWorld());
+
+            instance.setWorthiness(instance.getWorthiness(playerUuid) - 750);
+            instance.setRebuilt(hasRebuilt);
+
+            if (instance instanceof ServerConquestInstance) {
+                ServerConquestInstance serverConquestInstance = (ServerConquestInstance) instance;
+                serverConquestInstance.sync(player, null);
+            }
+
+            TaleOfKingdoms.LOGGER.debug("Guild attack forcefully activated!");
+            return 1;
+        } catch (CommandSyntaxException e) {
+            e.printStackTrace();
+            TaleOfKingdoms.LOGGER.debug("Guild attack forceful activation failed!");
+            return 0;
+        }
+    }
+}
