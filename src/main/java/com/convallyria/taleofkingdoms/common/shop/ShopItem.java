7b5cb49a55c3f4401de45fb52ef2b95f822e588b
Jordan Adams <JordanPlayz158again@gmail.com>
2021-06-05 19:09:44 -0400

    Implemented json to ShopItem conversion: - Removed all class-based ShopItem's - Updated BlacksmithEntity and FoodShopEntity to retrieve the list of ShopItem's from the ShopParser - Updated SellScreenHandler to work with new system - Merged ShopItem classes and values.json into shop.json - ShopParser will replace "_" with spaces in item if there is no name defined for the name (saves json space/redundancy) (Make documentation) - Updated ShopItem to work with new system - Removed unused imports in TaleOfKingdoms as well as removed itemValues map (handled sell items) as it was superseded by the new system - Fixes issue 56

--- src/main/java/com/convallyria/taleofkingdoms/common/shop/ShopItem.java
+++ src/main/java/com/convallyria/taleofkingdoms/common/shop/ShopItem.java
@@ -4,15 +4,46 @@ import com.convallyria.taleofkingdoms.common.world.ConquestInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 
-public abstract class ShopItem {
+public class ShopItem {
+    private final String name;
+    private final Item item;
+    private final int cost;
+    private final int sell;
 
-    public abstract int getCost();
+    public ShopItem(String name, Item item, int cost, int sell) {
+        this.name = name;
+        this.item = item;
+        this.cost = cost;
+        this.sell = sell;
+    }
 
-    public abstract Item getItem();
+    public String getName() {
+        return name;
+    }
 
-    public abstract String getName();
+    public Item getItem() {
+        return item;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getSell() {
+        return sell;
+    }
 
     public boolean canBuy(ConquestInstance instance, PlayerEntity player, int count) {
         return instance.getCoins(player.getUuid()) >= (getCost() * count);
+    }
+
+    @Override
+    public String toString() {
+        return "ShopItem{" +
+                "name='" + name + '\'' +
+                ", item=" + item +
+                ", cost=" + cost +
+                ", sell=" + sell +
+                '}';
     }
 }
