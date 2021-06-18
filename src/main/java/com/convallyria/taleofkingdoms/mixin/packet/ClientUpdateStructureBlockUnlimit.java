092985cc3f4c9c89bd093cf136eb00aae6575acf
SamB440 <sam@islandearth.net>
2021-06-18 19:35:41 +0100

    Fixes #61
    Signed-off-by: SamB440 <sam@islandearth.net>

--- src/main/java/com/convallyria/taleofkingdoms/mixin/packet/ClientUpdateStructureBlockUnlimit.java
+++ src/main/java/com/convallyria/taleofkingdoms/mixin/packet/ClientUpdateStructureBlockUnlimit.java
@@ -5,15 +5,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(value = UpdateStructureBlockC2SPacket.class, priority = 1001) // Carpet support - use ours
+@Mixin(value = UpdateStructureBlockC2SPacket.class, priority = 999) // Carpet support - use ours
 public class ClientUpdateStructureBlockUnlimit {
     
-    @ModifyConstant(method = "read", constant = @Constant(intValue = 48))
+    @ModifyConstant(method = "read", constant = @Constant(intValue = 48), require = 0)
     public int reinitUpper(int value) {
         return 512;
     }
     
-    @ModifyConstant(method = "read", constant = @Constant(intValue = -48))
+    @ModifyConstant(method = "read", constant = @Constant(intValue = -48), require = 0)
     public int reinitLower(int value) {
         return -512;
     }
--- src/main/java/com/convallyria/taleofkingdoms/mixin/structure/StructureBlockUnlimit.java
+++ src/main/java/com/convallyria/taleofkingdoms/mixin/structure/StructureBlockUnlimit.java
@@ -7,15 +7,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(value = StructureBlockBlockEntity.class, priority = 1001) // Carpet support - use ours
+@Mixin(value = StructureBlockBlockEntity.class, priority = 999) // Carpet support - use ours
 public class StructureBlockUnlimit {
     
-    @ModifyConstant(method = "fromTag", constant = @Constant(intValue = 48))
+    @ModifyConstant(method = "fromTag", constant = @Constant(intValue = 48), require = 0)
     public int readNbtUpper(int value) {
         return 512;
     }
     
-    @ModifyConstant(method = "fromTag", constant = @Constant(intValue = -48))
+    @ModifyConstant(method = "fromTag", constant = @Constant(intValue = -48), require = 0)
     public int readNbtLower(int value) {
         return -512;
     }
