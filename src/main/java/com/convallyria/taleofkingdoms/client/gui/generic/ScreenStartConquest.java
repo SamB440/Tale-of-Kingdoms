d131c2d0e186a7e69b293d91761a6cc41772e3f4
SamB440 <sam@islandearth.net>
2022-07-13 21:52:55 +0100

    Fix guild spawning in ground too far

--- src/main/java/com/convallyria/taleofkingdoms/client/gui/generic/ScreenStartConquest.java
+++ src/main/java/com/convallyria/taleofkingdoms/client/gui/generic/ScreenStartConquest.java
@@ -80,7 +80,7 @@ public class ScreenStartConquest extends ScreenTOK {
             }
             api.getConquestInstanceStorage().addConquest(worldName, instance, true);
 
-            BlockPos pastePos = serverPlayer.getBlockPos().subtract(new Vec3i(0, 26, 0));
+            BlockPos pastePos = serverPlayer.getBlockPos().subtract(new Vec3i(0, 20, 0));
             api.getSchematicHandler().pasteSchematic(Schematic.GUILD_CASTLE, serverPlayer, pastePos).thenAccept(oi -> api.executeOnServer(() -> {
                 BlockPos start = new BlockPos(oi.getMaxX(), oi.getMaxY(), oi.getMaxZ());
                 BlockPos end = new BlockPos(oi.getMinX(), oi.getMinY(), oi.getMinZ());
