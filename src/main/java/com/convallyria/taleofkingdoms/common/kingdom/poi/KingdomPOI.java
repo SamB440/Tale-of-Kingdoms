ca273249b25cd71fb54fd5e52162da8823426f8f
SamB440 <sam@islandearth.net>
2022-11-07 14:45:46 +0000

    Add tier one blacksmith house

--- src/main/java/com/convallyria/taleofkingdoms/common/kingdom/poi/KingdomPOI.java
+++ src/main/java/com/convallyria/taleofkingdoms/common/kingdom/poi/KingdomPOI.java
@@ -2,23 +2,38 @@ package com.convallyria.taleofkingdoms.common.kingdom.poi;
 
 import com.convallyria.taleofkingdoms.TaleOfKingdoms;
 import com.convallyria.taleofkingdoms.common.kingdom.PlayerKingdom;
+import com.convallyria.taleofkingdoms.common.schematic.Schematic;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.structure.StructureTemplate;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
-public enum KingdomPOI implements POIProcessor {
-    CITY_BUILDER_WELL_POI("CityBuilderWellPOI") {
-        @Override
-        public void compute(PlayerKingdom kingdom, StructureTemplate.StructureBlockInfo info) {
-            kingdom.addPOI(CITY_BUILDER_WELL_POI, info.pos);
-            TaleOfKingdoms.LOGGER.info("Found city builder well POI @ " + info.pos);
-        }
-    };
+public enum KingdomPOI {
+    CITY_BUILDER_WELL_POI("CityBuilderWellPOI"),
+    TIER_ONE_HOUSE_BLACKSMITH("TierOneHouseBlacksmith");
 
     private final String poiName;
+    private final Schematic schematic;
 
     KingdomPOI(String poiName) {
+        this(poiName, null);
+    }
+
+    KingdomPOI(String poiName, @Nullable Schematic schematic) {
         this.poiName = poiName;
+        this.schematic = schematic;
+    }
+
+    public void compute(PlayerKingdom kingdom, StructureTemplate.StructureBlockInfo info) {
+        kingdom.addPOI(this, info.pos);
+        TaleOfKingdoms.LOGGER.info("Found '" + poiName + "' POI @ " + info.pos);
+    }
+
+    public void place(ServerPlayerEntity player, PlayerKingdom kingdom) {
+        if (schematic != null) {
+            TaleOfKingdoms.getAPI().getSchematicHandler().pasteSchematic(schematic, player, kingdom.getPOIPos(this));
+        }
     }
 
     public String getPoiName() {
--- src/main/java/com/convallyria/taleofkingdoms/common/kingdom/poi/POIProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.convallyria.taleofkingdoms.common.kingdom.poi;
-
-import com.convallyria.taleofkingdoms.common.kingdom.PlayerKingdom;
-import net.minecraft.structure.StructureTemplate;
-
-public interface POIProcessor {
-
-    void compute(PlayerKingdom kingdom, StructureTemplate.StructureBlockInfo info);
-}
