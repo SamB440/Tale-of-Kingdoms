5d84a483c0287b6a5c1b495b694540a22ec6befd
SamB440 <sam@islandearth.net>
2022-11-09 21:28:57 +0000

    Organise build buttons into columns and abstract

--- src/main/java/com/convallyria/taleofkingdoms/common/kingdom/builds/BuildCosts.java
+++ src/main/java/com/convallyria/taleofkingdoms/common/kingdom/builds/BuildCosts.java
@@ -1,12 +1,14 @@
 package com.convallyria.taleofkingdoms.common.kingdom.builds;
 
+import com.convallyria.taleofkingdoms.common.kingdom.poi.KingdomPOI;
+import com.convallyria.taleofkingdoms.common.schematic.Schematic;
 import net.minecraft.text.Text;
 
 //todo: translatable
 public enum BuildCosts {
     SMALL_HOUSE(Text.literal("Small Houses"), 192, 128),
     LARGE_HOUSE(Text.literal("Large Houses"), 192, 320),
-    ITEM_SHOP(Text.literal("Item Shop"), 256, 256),
+    ITEM_SHOP(Text.literal("Item Shop"), Schematic.TIER_1_BLACKSMITH_HOUSE, KingdomPOI.TIER_ONE_HOUSE_BLACKSMITH, 256, 256),
     STOCK_MARKET(Text.literal("Stock Market"), 192, 192),
     BUILDER_HOUSE(Text.literal("Builder House"), 128, 128),
     BLOCK_SHOP(Text.literal("Block Shop"), 256, 320),
@@ -18,16 +20,36 @@ public enum BuildCosts {
     MAGE_HALL(Text.literal("Mage Hall"), 256, 320);
 
     private final Text displayName;
+    private final Schematic schematic;
+    private final KingdomPOI kingdomPOI;
     private final int wood, stone;
 
     BuildCosts(Text displayName, int wood, int stone) {
         this.displayName = displayName;
+        this.schematic = null;
+        this.kingdomPOI = null;
+        this.wood = wood;
+        this.stone = stone;
+    }
+
+    BuildCosts(Text displayName, Schematic schematic, KingdomPOI poi, int wood, int stone) {
+        this.displayName = displayName;
+        this.schematic = schematic;
+        this.kingdomPOI = poi;
         this.wood = wood;
         this.stone = stone;
     }
 
     public Text getDisplayName() {
         return displayName;
+    }
+
+    public Schematic getSchematic() {
+        return schematic;
+    }
+
+    public KingdomPOI getKingdomPOI() {
+        return kingdomPOI;
     }
 
     public int getWood() {
