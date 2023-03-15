20da7bbaa7c88fae8673e58c2ef75c2f280c21cb
SamB440 <sam@islandearth.net>
2023-03-15 21:20:22 +0000

    Bump version and fix shields in readme

--- src/main/java/com/convallyria/taleofkingdoms/TaleOfKingdoms.java
+++ src/main/java/com/convallyria/taleofkingdoms/TaleOfKingdoms.java
@@ -79,7 +79,7 @@ public class TaleOfKingdoms implements ModInitializer {
 
     public static final String MODID = "taleofkingdoms";
     public static final String NAME = "Tale of Kingdoms";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.0.5";
 
     public static final Logger LOGGER = LogManager.getLogger();
 
