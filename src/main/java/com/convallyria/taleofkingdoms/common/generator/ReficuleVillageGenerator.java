b4f71713ad19c54b6806abf8ed580399e37edf4d
SamB440 <sam@islandearth.net>
2023-05-28 14:04:27 +0100

    Add loot tables to reficule village

--- src/main/java/com/convallyria/taleofkingdoms/common/generator/ReficuleVillageGenerator.java
+++ src/main/java/com/convallyria/taleofkingdoms/common/generator/ReficuleVillageGenerator.java
@@ -1,8 +1,8 @@
 package com.convallyria.taleofkingdoms.common.generator;
 
-import com.convallyria.taleofkingdoms.common.generator.structure.TOKStructures;
 import com.convallyria.taleofkingdoms.TaleOfKingdoms;
 import com.convallyria.taleofkingdoms.common.entity.EntityTypes;
+import com.convallyria.taleofkingdoms.common.generator.structure.TOKStructures;
 import com.convallyria.taleofkingdoms.common.utils.EntityUtils;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.structure.SimpleStructurePiece;
@@ -16,7 +16,6 @@ import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockBox;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.ServerWorldAccess;
@@ -32,34 +31,27 @@ public class ReficuleVillageGenerator {
     private static final Identifier TOWER = new Identifier(TaleOfKingdoms.MODID, "reficule_village/reficule_village_tower");
 
     public static void addPieces(StructureTemplateManager manager, BlockPos pos, BlockRotation blockRotation, StructurePiecesHolder structurePiecesHolder, Random random) {
-        final Direction direction = Direction.random(random);
-        ReficuleVillagePiece onePiece = new ReficuleVillagePiece(manager, ONE, pos, BlockRotation.NONE, 0);
-        onePiece.setOrientation(direction);
+        ReficuleVillagePiece onePiece = new ReficuleVillagePiece(manager, ONE, pos.subtract(new Vec3i(0, 6, 0)), BlockRotation.NONE, 0);
         structurePiecesHolder.addPiece(onePiece);
 
         BlockPos middlePos = pos.add(new Vec3i(48, 0, 0));
         ReficuleVillagePiece middlePiece = new ReficuleVillagePiece(manager, MIDDLE, middlePos, BlockRotation.NONE, 0);
-        middlePiece.setOrientation(direction);
         structurePiecesHolder.addPiece(middlePiece);
 
         BlockPos threePos = middlePos.add(new Vec3i(32, 0, 0));
         ReficuleVillagePiece threePiece = new ReficuleVillagePiece(manager, THREE, threePos, BlockRotation.NONE, 0);
-        threePiece.setOrientation(direction);
         structurePiecesHolder.addPiece(threePiece);
 
         BlockPos middleTwoPos = middlePos.subtract(new Vec3i(0, 0, 36));
         ReficuleVillagePiece middleTwoPiece = new ReficuleVillagePiece(manager, MIDDLE_TWO, middleTwoPos, BlockRotation.NONE, 0);
-        middleTwoPiece.setOrientation(direction);
         structurePiecesHolder.addPiece(middleTwoPiece);
 
         BlockPos fourPos = middleTwoPos.add(new Vec3i(0, 0, 13)).add(new Vec3i(32, 0, 0));
         ReficuleVillagePiece fourPiece = new ReficuleVillagePiece(manager, FOUR, fourPos, BlockRotation.NONE, 0);
-        fourPiece.setOrientation(direction);
         structurePiecesHolder.addPiece(fourPiece);
 
         BlockPos towerPos = pos.subtract(new Vec3i(0, 0, 32));
         ReficuleVillagePiece towerPiece = new ReficuleVillagePiece(manager, TOWER, towerPos, BlockRotation.NONE, 0);
-        towerPiece.setOrientation(direction);
         structurePiecesHolder.addPiece(towerPiece);
     }
 
--- src/main/java/com/convallyria/taleofkingdoms/datagen/loottables/TOKChestLootTableGenerator.java
+++ src/main/java/com/convallyria/taleofkingdoms/datagen/loottables/TOKChestLootTableGenerator.java
@@ -36,5 +36,57 @@ public class TOKChestLootTableGenerator implements LootTableGenerator {
 								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(4, 32))))
 						.rolls(UniformLootNumberProvider.create(2, 8))
 		));
+
+		exporter.accept(TOKLootTables.REFICULE_VILLAGE_WELL, LootTable.builder().pool(
+				LootPool.builder()
+						.bonusRolls(ConstantLootNumberProvider.create(0))
+						.with(ItemEntry.builder(Items.ARROW).weight(14)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(2, 4))))
+						.with(ItemEntry.builder(Items.COD).weight(12)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(3, 8))))
+						.with(ItemEntry.builder(Items.STONE_AXE).weight(9)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 1))))
+						.with(ItemEntry.builder(Items.CHAINMAIL_CHESTPLATE).weight(6)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 1))))
+						.with(ItemEntry.builder(Items.IRON_INGOT).weight(3)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(2, 7))))
+						.rolls(UniformLootNumberProvider.create(3, 7))
+		));
+
+		exporter.accept(TOKLootTables.REFICULE_VILLAGE_HOUSE, LootTable.builder().pool(
+				LootPool.builder()
+						.bonusRolls(ConstantLootNumberProvider.create(0))
+						.with(ItemEntry.builder(Items.COAL).weight(20)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(2, 12))))
+						.with(ItemEntry.builder(Items.BREAD).weight(12)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(3, 8))))
+						.with(ItemEntry.builder(Items.HONEY_BOTTLE).weight(10)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 2))))
+						.with(ItemEntry.builder(Items.HONEYCOMB).weight(8)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 3))))
+						.with(ItemEntry.builder(Items.IRON_PICKAXE).weight(6)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 1))))
+						.with(ItemEntry.builder(Items.IRON_SWORD).weight(4)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 1))))
+						.rolls(UniformLootNumberProvider.create(3, 7))
+		));
+
+		exporter.accept(TOKLootTables.REFICULE_VILLAGE_TOWER, LootTable.builder().pool(
+				LootPool.builder()
+						.bonusRolls(ConstantLootNumberProvider.create(0))
+						.with(ItemEntry.builder(Items.ARROW).weight(14)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(4, 16))))
+						.with(ItemEntry.builder(Items.TORCH).weight(12)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(2, 12))))
+						.with(ItemEntry.builder(Items.BREAD).weight(10)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(3, 6))))
+						.with(ItemEntry.builder(Items.APPLE).weight(10)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(4, 8))))
+						.with(ItemEntry.builder(Items.IRON_SHOVEL).weight(8)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1, 1))))
+						.with(ItemEntry.builder(Items.IRON_INGOT).weight(4)
+								.apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(3, 8))))
+						.rolls(UniformLootNumberProvider.create(3, 7))
+		));
 	}
 }
