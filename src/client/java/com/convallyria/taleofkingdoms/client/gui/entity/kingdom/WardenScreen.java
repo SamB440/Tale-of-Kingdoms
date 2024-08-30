5c5734bf5a85b2018db0c5132a61c10ec42a54be
SamB440 <sam@islandearth.net>
2024-08-30 21:35:21 +0100

    Rework warden UI

--- src/client/java/com/convallyria/taleofkingdoms/client/gui/entity/kingdom/WardenScreen.java
+++ src/client/java/com/convallyria/taleofkingdoms/client/gui/entity/kingdom/WardenScreen.java
@@ -1,31 +1,24 @@
 package com.convallyria.taleofkingdoms.client.gui.entity.kingdom;
 
+import com.convallyria.taleofkingdoms.TaleOfKingdoms;
 import com.convallyria.taleofkingdoms.common.entity.kingdom.warden.WardenEntity;
 import com.convallyria.taleofkingdoms.common.world.ConquestInstance;
-import io.wispforest.owo.ui.base.BaseOwoScreen;
+import io.wispforest.owo.ui.base.BaseUIModelScreen;
 import io.wispforest.owo.ui.component.ButtonComponent;
-import io.wispforest.owo.ui.component.Components;
-import io.wispforest.owo.ui.container.Containers;
+import io.wispforest.owo.ui.component.LabelComponent;
 import io.wispforest.owo.ui.container.FlowLayout;
-import io.wispforest.owo.ui.core.Color;
-import io.wispforest.owo.ui.core.HorizontalAlignment;
-import io.wispforest.owo.ui.core.OwoUIAdapter;
-import io.wispforest.owo.ui.core.Positioning;
-import io.wispforest.owo.ui.core.Sizing;
-import io.wispforest.owo.ui.core.Surface;
-import io.wispforest.owo.ui.core.VerticalAlignment;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.util.DyeColor;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.util.Identifier;
 
-public class WardenScreen extends BaseOwoScreen<FlowLayout> {
+public class WardenScreen extends BaseUIModelScreen<FlowLayout> {
 
     private final PlayerEntity player;
     private final WardenEntity entity;
     private final ConquestInstance instance;
 
     public WardenScreen(PlayerEntity player, WardenEntity entity, ConquestInstance instance) {
+        super(FlowLayout.class, BaseUIModelScreen.DataSource.asset(Identifier.of(TaleOfKingdoms.MODID, "warden_ui_model")));
         this.player = player;
         this.entity = entity;
         this.instance = instance;
@@ -33,35 +26,17 @@ public class WardenScreen extends BaseOwoScreen<FlowLayout> {
     }
 
     @Override
-    protected @NotNull OwoUIAdapter<FlowLayout> createAdapter() {
-        return OwoUIAdapter.create(this, Containers::horizontalFlow);
-    }
-
-    @Override
     protected void build(FlowLayout rootComponent) {
-        rootComponent
-                .surface(Surface.VANILLA_TRANSLUCENT)
-                .horizontalAlignment(HorizontalAlignment.CENTER)
-                .verticalAlignment(VerticalAlignment.CENTER);
+        final FlowLayout inner = rootComponent.childById(FlowLayout.class, "inner");
 
-        rootComponent.child(
-            Components.label(Text.translatable("menu.taleofkingdoms.warden.total_money", instance.getPlayer(player).getCoins()))
-                    .color(Color.ofDye(DyeColor.WHITE))
-                    .positioning(Positioning.relative(50, 15))
-        );
+        inner.childById(LabelComponent.class, "total-money").text(Text.translatable("menu.taleofkingdoms.warden.total_money", instance.getPlayer(player).getCoins()));
 
-        rootComponent.child(
-            Components.button(Text.translatable("menu.taleofkingdoms.warden.recruit.knight"), c -> {
-                entity.buySoldier(player, instance, (byte) 1);
-            })
-            .tooltip(Text.translatable("menu.taleofkingdoms.warden.buy_cost"))
-            .sizing(Sizing.content(), Sizing.fixed(20))
-            .positioning(Positioning.relative(50, 45))
-        );
+        inner.childById(ButtonComponent.class, "recruit-knight-button").onPress(b -> {
+            entity.buySoldier(player, instance, (byte) 1);
+        });
 
-        rootComponent.child(
-            Components.button(Text.translatable("menu.taleofkingdoms.warden.recruit.archer"), c -> {
-                entity.buySoldier(player, instance, (byte) 2);
+        inner.childById(ButtonComponent.class, "recruit-archer-button").onPress(b -> {
+            entity.buySoldier(player, instance, (byte) 2);
 //                if (MinecraftClient.getInstance().getServer() == null) {
 //                    TaleOfKingdomsClient.getAPI().getClientPacketHandler(Packets.FOREMAN_BUY_WORKER)
 //                            .handleOutgoingPacket(player, entity.getId());
@@ -69,32 +44,19 @@ public class WardenScreen extends BaseOwoScreen<FlowLayout> {
 //                }
 //
 //                entity.buyWorker(player, instance);
-            })
-            .tooltip(Text.translatable("menu.taleofkingdoms.warden.buy_cost"))
-            .sizing(Sizing.content(), Sizing.fixed(20))
-            .positioning(Positioning.relative(50, 55))
-        );
+        });
 
-        rootComponent.child(
-            Components.button(Text.translatable("menu.taleofkingdoms.warden.recall"), c -> {
+        inner.childById(ButtonComponent.class, "recall-defenders-button").onPress(b -> {
 //                if (MinecraftClient.getInstance().getServer() == null) {
 //                    TaleOfKingdomsClient.getAPI().getClientPacketHandler(Packets.FOREMAN_BUY_WORKER)
 //                            .handleOutgoingPacket(player, entity.getId());
 //                    return;
 //                }
 //
-                entity.recallSoldiers(player, instance);
-            })
-            .sizing(Sizing.content(), Sizing.fixed(20))
-            .positioning(Positioning.relative(50, 65))
-        );
+            entity.recallSoldiers(player, instance);
+        });
 
-        rootComponent.child(
-            Components.button(
-                Text.translatable("menu.taleofkingdoms.generic.exit"),
-                (ButtonComponent button) -> this.close()
-            ).positioning(Positioning.relative(50, 85))
-        );
+        inner.childById(ButtonComponent.class, "exit-button").onPress(b -> this.close());
     }
 
     @Override
